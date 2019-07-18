e9ca5a8276cb0c4e7e0e6f41e65f10140936d52e
OuuoJun <2276181401@qq.com>
2019-07-18 10:16:36 +0800

    init project

---
+++ src/main/java/com/example/gitdemo/GitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitdemoApplication.class, args);
+    }
+
+}
